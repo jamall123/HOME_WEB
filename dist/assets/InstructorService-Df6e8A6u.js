364d880e95d76cff2ebd570f930759f5f8cceca3
Jamal Ahmed <ja1827082@gmail.com>
2026-07-31 09:00:26 +0200

    Fix Vite dynamic import by removing runtime cache buster

---
+++ dist/assets/InstructorService-Df6e8A6u.js
@@ -0,0 +1,1 @@
+import{t as s}from"./InstructorController-DDcP-lCF.js";export{s as InstructorService};
--- dist/assets/InstructorService-DtUS7zBd.js
+++
@@ -1,1 +0,0 @@
-import{t as s}from"./InstructorController-DqTUoMBi.js";export{s as InstructorService};
